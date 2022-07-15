c1e29ae4dba782e0834bad807ece7d3cfc051323
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2022-07-15 17:31:40 -0230

    SignupScreen: created

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,46 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./LoginScreen.css"
+import SignUpScreen from './SignUpScreen'
 
 function LoginScreen() {
+    const [signIn, setSignIn] = useState(false)
+
+
+
     return (
         <div className="loginScreen">
-            <div className="loginScreen__background">
-                <img
-                    className="loginScreen__logo"
-                    src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
-                    alt=""
-                />
-
-                <button
-                    className="loginScreen__button">
-                    Sign In
-                </button>
-
-                <div className="loginScreen__gradient"></div>
-            </div>
-
-            {/*  Login Screen body starts */}
-            <div className="loginScreen__body">
+            { signIn ? (
+                <SignUpScreen />
+            ) :
                 <>
-                    <h1>Unlimited films, TV programs and more.</h1>
-                    <h2>Watch anywhere. Cancel at any time.</h2>
-                    <h3>
-                        Ready to watch? Enter your email to create or restart your membership.
-                    </h3>
+                    <div className="loginScreen__background">
+                        <img
+                            className="loginScreen__logo"
+                            src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+                            alt=""
+                        />
+
+                        <button
+                            onClick={() => setSignIn(true)}
+                            className="loginScreen__button">
+                            Sign In
+                        </button>
+
+                        <div className="loginScreen__gradient"></div>
+                    </div>
+
+                    <div className="loginScreen__body">
+                        <>
+                            <h1>Unlimited films, TV programs and more.</h1>
+                            <h2>Watch anywhere. Cancel at any time.</h2>
+                            <h3>
+                                Ready to watch? Enter your email to create or restart your membership.
+                            </h3>
 
 
-                    <div className="loginScreen__input">
-                        <form>
-                            <input
-                                type="email"
-                                className="loginScreen__email"
-                                placeholder="Email Address"/>
+                            <div className="loginScreen__input">
+                                <form>
+                                    <input
+                                        type="email"
+                                        className="loginScreen__email"
+                                        placeholder="Email Address"/>
 
-                            <button className="loginScreen__getStarted">Get Started</button>
-                        </form>
+                                    <button
+                                        onClick={() => setSignIn(true)}
+                                        className="loginScreen__getStarted">Get Started</button>
+                                </form>
+                            </div>
+                        </>
                     </div>
                 </>
-            </div>
+            }
 
         </div>
     );
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignUpScreen(props) {
+    return (
+        <div children="signUpScreen">
+            <h1>Sign Up Screen</h1>
+        </div>
+    );
+}
+
+export default SignUpScreen;
