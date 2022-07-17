571b437593c275afe29b236b77643a412ed0f60d
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2022-07-17 16:15:32 -0230

    LoginScreen: include the gradient

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -10,6 +10,13 @@ function LoginScreen() {
                     src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
                     alt=""
                 />
+
+                <button
+                    className="loginScreen__button">
+                    Sign In
+                </button>
+
+                <div className="loginScreen__gradient"></div>
             </div>
         </div>
     );
