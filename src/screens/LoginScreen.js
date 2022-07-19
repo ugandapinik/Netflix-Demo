59880e4ea21fb08c6c3f5dce7ea43dcd3567d997
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2022-07-19 15:45:51 -0230

    LoginScreen: update the logo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,8 +3,14 @@ import "./LoginScreen.css"
 
 function LoginScreen() {
     return (
-        <div className="LoginScreen">
-            <h1>Login Screen</h1>
+        <div className="loginScreen">
+            <div className="loginScreen__background">
+                <img
+                    className="loginScreen__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+                    alt=""
+                />
+            </div>
         </div>
     );
 }
