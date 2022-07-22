4c1b2418bd7ec1d5ef0c5c483314334ef47a5b98
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2022-07-22 21:11:46 -0230

    Update Nav bar background

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import './Nav.css'
 
 function Nav() {
   return (
-    <div className="nav">
+    <div className="nav nav__black">
         <div className="nav__contents">
             <img 
                 className="nav__logo"
