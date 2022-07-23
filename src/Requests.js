bc8658f2abeb4963b8f5f665fa164caba71f060e
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2022-07-23 01:46:19 -0230

    Include requests.js file

---
+++ src/Requests.js
@@ -0,0 +1,13 @@
+// store in {process.env.API_KEY}
+const API_KEY = "3aa1a4aeb38b67696f8f502fd2508089"
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
