88f5dbefc799979a21b0c39e79e01ceaf3d799f5
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2022-07-27 18:13:24 -0230

    include React-router-dom libary for the navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen' 
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+        <HomeScreen />
+
+        <Router>
+            <Switch>
+                <Route path="/about"><About /></Route>
+                <Route path="/users"><Users /></Route>
+                <Route path="/"><Home /></Route>
+            </Switch>
+        </Router>
+
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,8 @@
-var firebase = "Demo";
+const firebaseConfig = {
+    apiKey: "AIzaSyCYh_f2im5xCfaqY1RKkhrkiIyi1Iaj7ms",
+    authDomain: "netflix-clone-d9515.firebaseapp.com",
+    projectId: "netflix-clone-d9515",
+    storageBucket: "netflix-clone-d9515.appspot.com",
+    messagingSenderId: "541776291256",
+    appId: "1:541776291256:web:1746490f6bf170f4b85b4d"
+};
